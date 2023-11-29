a7b33ca78f5970244a56cc32d9baa57f5194d81d
choriyeva <rozigulchoriyeva15@gmail.com>
2023-11-29 17:46:05 +0500

    watch

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ setInterval(displayTime,1000);
 
 
 
+
+
+document.onkeydown = (e) => {
+    if (e.key == 123) {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'I') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'C') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.shiftKey && e.key == 'J') {
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key == 'U') {
+        e.preventDefault();
+    }
+};
+
+
